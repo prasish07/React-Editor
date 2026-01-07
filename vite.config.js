cbf0f77d11ad3dce71e6a830eafaf85cf93a00c1
prasish07 <prasishshrestha0099@gmail.com>
2026-01-07 14:47:55 +0545

    Enable cross origin embedded policy

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
   },
+  preview: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
   optimizeDeps: {
     exclude: ['@webcontainer/api'],
   },
